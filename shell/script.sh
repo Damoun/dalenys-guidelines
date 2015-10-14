1c50b2e05448e71361acc16a9a775d992ff84d2d
Damien Plenard <damien@plenard.me>
2015-10-14 22:00:17 +0200

    replace rentabiliweb by dalenys in copyright header in shell script

--- shell/script.sh
+++ shell/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2015, Rentabiliweb Group
+# Copyright (c) 2015, Dalenys
 #
 # Permission  to use,  copy, modify,  and/or  distribute this  software for  any
 # purpose  with  or without  fee  is hereby  granted,  provided  that the  above
